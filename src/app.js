ae507b2490ff536ee262000ef944652125bb390c
Katie S <kdshepard89@gmail.com>
2023-02-10 15:16:12 -0500

    created css, js, html

---
+++ src/app.js
@@ -0,0 +1,49 @@
+let now = new Date();
+
+let h3 = document.querySelector("h3");
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hours = now.getHours();
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = "0" + minutes;
+}
+h3.innerHTML = day + " " + hours + ":" + minutes;
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city-name").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function searchCity(city) {
+  let apiKey = "b1a8336ff1e05b64da5625e4158fbea3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city").value;
+  searchCity(city);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
