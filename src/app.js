c7b1643613eea0ba25a5cc2c0569047dabe48c9d
Katie S <kdshepard89@gmail.com>
2023-02-16 10:03:51 -0500

    threw out forecast attempt, changed CSS styling, finished project

--- src/app.js
+++ src/app.js
@@ -18,54 +18,6 @@ if (minutes < 10) {
   minutes = "0" + minutes;
 }
 h3.innerHTML = day + " " + hours + ":" + minutes;
-function displayForecast(response) {
-  let forecast = response.data.daily;
-
-  let forecastElement = document.querySelector("#forecast");
-
-  let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
- <div class="col-2">
- <div class="weather-forecast-date">${day}</div>
- <img 
- src="http://openweathermap.org/img/wn/02d.png"
- alt=""
- width="42"
- />
- <div class="weather-forecast-temperatures">
- <span class"weather-forecast-temperature-max"> 18° </span>
- <span class="weather-forecast-temperature-min"> 12° </span>
- </div>
-`;
-  });
-  forecastHTML =
-    forecastHTML +
-    `
-<div class="col-2">
- <div class="weather-forecast-date">Thu</div>
- <img 
- src="http://openweathermap.org/img/wn/02d.png"
- alt=""
- width="42"
- />
- <div class="weather-forecast-temperatures">
- <span class"weather-forecast-temperature-max"> 18° </span>
- <span class="weather-forecast-temperature-min"> 12° </span>
- </div>
-`;
-  forecastHTML = forecastHTML + `</div>`;
-  forecastElement.innerHTML = forecastHTML;
-}
-function getForecast(coordinates) {
-  let apiKey = "b1a8336ff1e05b64da5625e4158fbea3";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?
-  lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unites=metric`;
-  axios.get(apiUrl).then("#forecast");
-}
 
 function displayWeatherCondition(response) {
   document.querySelector("#city-name").innerHTML = response.data.name;
@@ -88,8 +40,6 @@ function displayWeatherCondition(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
-
-  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
